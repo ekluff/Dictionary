aa31045a1e2bdd4e37d8d962723eb1225e6bc9f6
Evan Clough <clough.evan@gmail.com>
2015-08-14 10:21:53 -0700

    Expand class Word method initialize spec to test automatic assignment of attributes including id, creation time, definitions = [], and include_in_dictionary = true.

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -10,10 +10,16 @@ describe('Word') do
   end
 
   describe('#initialize') do
-    it('creates a new instance of class Word') do
+    it('creates a new instance of class Word that accepts user input word, assigns ID, saves a creation time, sets include_in_dictionary to true, and creates nested empty array @definitions.') do
       test_word = Word.new({:word => 'tacocat'})
       expect(test_word.word).to(eq('tacocat'))
+      expect(test_word.id).to(eq(0))
+      expect(test_word.creation_time.class).to(eq(Time))
+      expect(test_word.include_in_dictionary).to(eq(true))
+      expect(test_word.definitions).to(eq([]))
     end
   end
+
+
 
 end
