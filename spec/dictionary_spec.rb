5ba1cf896e55659e55a5745d3823f29eccd95658
Evan Clough <clough.evan@gmail.com>
2015-08-14 10:00:24 -0700

    Write spec to test ability to create instances of class Word.

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,6 +8,12 @@ describe('Word') do
   before() do
     Word.clear
   end
-  
+
+  describe('#initialize') do
+    it('creates a new instance of class Word') do
+      test_word = Word.new({:word => 'tacocat'})
+      expect(test_word.word).to(eq('tacocat'))
+    end
+  end
 
 end
